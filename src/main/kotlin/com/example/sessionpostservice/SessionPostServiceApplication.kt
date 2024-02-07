dc90dedc65e9c7a94f3f1176030919102fe0f65a
changu100 <chad@101.inc>
2024-02-07 09:34:55 +0900

    project init

---
+++ src/main/kotlin/com/example/sessionpostservice/SessionPostServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sessionpostservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SessionPostServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SessionPostServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/sessionpostservice/SessionPostServiceApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.sessionpostservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SessionPostServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
